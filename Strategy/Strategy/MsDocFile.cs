fde812cb5c744d4c6b6cd98a25fc9d31ec00f880
fatematahir10 <122002682+fatematahir10@users.noreply.github.com>
2023-02-10 11:08:10 +0500

    Add files via upload

---
+++ Strategy/Strategy/MsDocFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    class MsDocFile
+    {
+        string compressedFileName;
+        public MsDocFile(string compressedFileName)
+        {
+            this.compressedFileName = compressedFileName;
+        }
+        public void Compress()
+        {
+            Console.WriteLine("Compressing doc " + compressedFileName);
+        }
+    }
+}
---
+++ Strategy/Strategy/textFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Strategy
+{
+    public class textFile : ICompression
+    {
+        string compressedFileName;
+        public textFile(string compressedFileName)
+        {
+            this.compressedFileName = compressedFileName;
+        }
+        public void Compress()
+        {
+            Console.WriteLine("Compressing text file " + compressedFileName);
+        }
+    }
+}
